94feb64eab11dce92bb81d01b58cba0173b4c9bc
abuzogan <aron.buzogany@ugent.be>
2023-08-08 14:56:56 +0200

    created base of app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,49 @@
-import './App.css'
+import "./App.css";
+import React from "react";
+import { Admin, ListGuesser, Resource, ShowGuesser } from "react-admin";
+import dataProvider from "./dataProvider/GDVDataProvider.js";
+import config from "./config";
+import SolidLoginForm from "./components/LoginPage/LoginPage";
+import { useEffect, useState } from "react";
+import {
+  getDefaultSession,
+  handleIncomingRedirect,
+} from "@inrupt/solid-client-authn-browser";
 
 function App() {
+  const session = getDefaultSession();
+  const [loggedIn, setLoggedIn] = useState();
 
+  useEffect(() => {
+    session.onLogin(() => setLoggedIn(true));
+    session.onLogout(() => setLoggedIn(false));
+
+    // In this function we don't use await because inside a React Effect it causes linting warnings and according to several sources on the Web it is not recommended.
+    // https://ultimatecourses.com/blog/using-async-await-inside-react-use-effect-hook
+    // https://www.thisdot.co/blog/async-code-in-useeffect-is-dangerous-how-do-we-deal-with-it/
+    handleIncomingRedirect({ restorePreviousSession: true }).then((info) => {
+      if (info) {
+        let status = info.isLoggedIn;
+        if (status !== loggedIn) {
+          setLoggedIn(status);
+        }
+      }
+    });
+  });
   return (
-    <>
-      
-    </>
-  )
+    <Admin dataProvider={dataProvider} loginPage={SolidLoginForm}>
+      {config.queries.map((query) => {
+        return (
+          <Resource
+            key={query.id}
+            name={query.id}
+            options={{ label: query.name }}
+            list={ListGuesser}
+          />
+        );
+      })}
+    </Admin>
+  );
 }
 
-export default App
+export default App;
