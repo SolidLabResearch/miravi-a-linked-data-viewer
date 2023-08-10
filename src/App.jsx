e6b99582336b7904f83d949481e9370247620f4b
abuzogan <aron.buzogany@ugent.be>
2023-08-10 17:14:58 +0200

    removed login page from admin

--- src/App.jsx
+++ src/App.jsx
@@ -36,7 +36,7 @@ function App() {
     });
   });
   return (
-    <Admin dataProvider={dataProvider} loginPage={SolidLoginForm}>
+    <Admin dataProvider={dataProvider}>
       {config.queries.map((query) => {
         return (
           <Resource
