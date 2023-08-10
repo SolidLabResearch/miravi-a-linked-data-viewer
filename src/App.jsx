7c10a75588430346c885f5c0f2f891f8692d068d
abuzogan <aron.buzogany@ugent.be>
2023-08-10 12:24:20 +0200

    added autprovider and loginpage

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import {
   handleIncomingRedirect,
 } from "@inrupt/solid-client-authn-browser";
 import GDVResource from "./components/GDVResource/GDVResource";
+import authenticationProvider from "./authenticationProvider/authenticationProvider";
 
 function App() {
   const session = getDefaultSession();
@@ -36,7 +37,7 @@ function App() {
     });
   });
   return (
-    <Admin dataProvider={dataProvider} loginPage={SolidLoginForm}>
+    <Admin dataProvider={dataProvider} loginPage={SolidLoginForm} authProvider={authenticationProvider}>
       {config.queries.map((query) => {
         return (
           <Resource
