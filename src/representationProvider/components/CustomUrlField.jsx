fa092effb0ffb66eb8fb05ac4b5052c40edda957
abuzogan <aron.buzogany@ugent.be>
2023-08-11 10:51:02 +0200

    being more specific with documentation

--- src/representationProvider/components/CustomUrlField.jsx
+++ src/representationProvider/components/CustomUrlField.jsx
@@ -7,7 +7,7 @@ import { Component } from "react";
  * @param {object} props the props passed down to the component
  * @param {object} props.record an object of RDF/JS object
  * @param {string} props.source the key of the object to be processed
- * @returns {Component} a component that displays the url of the given RDF/JS object
+ * @returns {Component} a component that displays the url of the given RDF/JS Term id 
  */
 function CustomURLField({ source, record }) {
   record[source] = getRawData(record, source);
