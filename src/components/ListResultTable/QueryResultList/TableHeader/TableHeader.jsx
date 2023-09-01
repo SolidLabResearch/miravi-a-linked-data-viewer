722e7433b7e92e410f062884afa5bc4ac3728130
Aron Buzogany <aronsaps@gmail.com>
2023-09-01 08:57:58 +0200

    created custom table header

--- src/components/ListResultTable/QueryResultList/TableHeader/TableHeader.jsx
+++ src/components/ListResultTable/QueryResultList/TableHeader/TableHeader.jsx
@@ -1,17 +1,22 @@
-import { Button, TableCell, TableHead, TableRow } from "@mui/material";
+import { Link, TableCell, TableHead, TableRow } from "@mui/material";
 import React from "react";
-import { useListSortContext } from "react-admin";
+import { useListContext } from "react-admin";
+import "./TableHeader.css";
+import NorthIcon from "@mui/icons-material/North";
+import SouthIcon from "@mui/icons-material/South";
+import LinkIcon from "@mui/icons-material/Link";
 
-function TableHeader({ children, sort }) {
-  const { currentSort, setSort } = useListSortContext();
-  console.log(currentSort)
-  function handleHeaderClick(target){
-    const newSort = {field: target, order: "DESC"};
-    if(currentSort){
-      if(currentSort.order === "ASC"){
+function TableHeader({ children, config }) {
+  const { sort, setSort, resource } = useListContext();
+  const { variableOntology } = config.queries.filter(
+    (query) => query.id === resource
+  )[0];
+  function handleHeaderClick(target) {
+    const newSort = { field: target, order: "DESC" };
+    if (sort) {
+      if (sort.order === "ASC") {
         newSort.order = "DESC";
-      }
-      else{
+      } else {
         newSort.order = "ASC";
       }
     }
@@ -21,11 +26,49 @@ function TableHeader({ children, sort }) {
   return (
     <TableHead>
       <TableRow>
-        <TableCell></TableCell>{" "}
+        <TableCell> </TableCell>
         {React.Children.map(children, (child) => (
           <>
-            <TableCell key={child.props.source}>
-                <Button variant="span" onClick={() => handleHeaderClick(child.props.source)}>{child.props.label}</Button>
+            <TableCell
+              key={child.props.source}
+              sx={{ height: "100%", "& > *": { verticalAlign: "middle" } }}
+            >
+              <span
+                variant="span"
+                role="button"
+                className="header-button"
+                onClick={() => handleHeaderClick(child.props.source)}
+              >
+                {child.props.label}
+              </span>
+              {variableOntology[child.props.source] && (
+                <Link
+                  target="_blank"
+                  href={variableOntology[child.props.source]}
+                  sx={{ height: "100%", margin: "0 5px", "& > *": { verticalAlign: "middle" } }}
+                >
+                  <LinkIcon
+                    fontSize="small"
+                    sx={{ height: "100%", color: "gray" }}
+                  />
+                </Link>
+              )}
+              {sort.field === child.props.source && (
+                <>
+                  {sort && sort.order === "DESC" && (
+                    <NorthIcon
+                      fontSize="small"
+                      sx={{ height: "100%", color: "gray" }}
+                    />
+                  )}
+                  {sort && sort.order === "ASC" && (
+                    <SouthIcon
+                      fontSize="small"
+                      sx={{ height: "100%", color: "gray" }}
+                    />
+                  )}
+                </>
+              )}
             </TableCell>
           </>
         ))}
