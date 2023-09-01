4f47f493077f05f04735f2e63a4a2c962ea9c6a5
Aron Buzogany <aronsaps@gmail.com>
2023-09-01 09:25:32 +0200

    updated proxy docs

--- src/components/ActionBar/SourceFetchStatusIcon/SourceFetchStatusIcon.jsx
+++ src/components/ActionBar/SourceFetchStatusIcon/SourceFetchStatusIcon.jsx
@@ -8,7 +8,7 @@ import { Component } from "react";
  * @param {object} props - the props passed to the component
  * @param {object} props.context - the query context
  * @param {string} props.source - the source to check
- * @param {string} props.proxyUrl - the proxy url to use if querying is used for sources
+ * @param {string} props.proxyUrl - the proxy url to use if the resource is accessed through a proxy
  * @returns {Component} an icon indicating whether the query was executed succesfully or not
  */
 function SourceFetchStatusIcon({ context, source, proxyUrl }) {
