34d4597f7f3b53a0b1b11c48ed673ad922922f88
Aron Buzogany <aronsaps@gmail.com>
2023-09-01 10:51:29 +0200

    added icon for musicians test query

--- src/IconProvider/IconProvider.js
+++ src/IconProvider/IconProvider.js
@@ -2,10 +2,12 @@ import BurstModeIcon from "@mui/icons-material/BurstMode";
 import BrushIcon from "@mui/icons-material/Brush";
 import MenuBookIcon from "@mui/icons-material/MenuBook";
 import BiotechIcon from "@mui/icons-material/Biotech";
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
 
 export default {
   images: BurstModeIcon,
   picasso: BrushIcon,
   books: MenuBookIcon,
   test: BiotechIcon,
+  music: MusicNoteIcon,
 };
