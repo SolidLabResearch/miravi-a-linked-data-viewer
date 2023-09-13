e0d28b0ca53547ac586928fa40901b0cd972486f
Pieter Heyvaert <pheyvaer.heyvaert@ugent.be>
2023-09-13 09:24:46 +0200

    Remove commented lines in script

--- scripts/configure-pods.sh
+++ scripts/configure-pods.sh
@@ -3,5 +3,3 @@
 copyfiles -u 1 -a "initial-pod-data/**/*" pods/example
 copyfiles -u 4 test/assets/webIdProfiles/noOidcIsser/* pods/example2/profile/
 copyfiles -u 4 test/assets/webIdProfiles/invalidWebId/* pods/invalidWebId/profile/
-#cpy assets/webIdProfiles/noOidcIsser/* pods/example2/profile/ --flat
-#cpy assets/WebIdProfiles/invalidWebId/* pods/invalidWebId/profile/ --flat
