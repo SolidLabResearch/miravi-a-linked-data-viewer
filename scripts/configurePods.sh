a6c08f62f1112227fabe684f12a3082eb61102b5
Pieter Heyvaert <pheyvaer.heyvaert@ugent.be>
2023-09-13 09:15:42 +0200

    Clean up

--- scripts/configurePods.sh
+++
@@ -1,5 +0,0 @@
-
-
-cpy initial-pod-data/* pods/example/ --flat && cpy initial-pod-data/images pods/example/images --flat
-cp assets/webIdProfiles/noOidcIsser/* pods/example2/profile/
-cp assets/WebIdProfiles/invalidWebId/* pods/invalidWebId/profile/
