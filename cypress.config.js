ea79af22c88963a991f69a253c15dc20221fb64b
Martin Vanbrabant <martin.vanbrabant@ugent.be>
2024-01-22 17:14:32 +0100

    Added test for templated queries

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
+    defaultCommandTimeout: 10000, /* is OK for very slow computers */
     baseUrl: 'http://localhost:5173',
     supportFile: false,
     video: false
