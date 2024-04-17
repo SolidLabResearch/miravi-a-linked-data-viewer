f84b8ff14bf451e5db3371d8cb07a082eb52352e
Ben De Meester <ben.demeester@ugent.be>
2024-04-17 15:21:21 +0200

    4 hours for 12 characters

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
   define: {
     // By default, Vite doesn't include shims for NodeJS/
     // necessary for crypto lib to work
-    global: {},
+    global: 'globalThis'
   },
 })
