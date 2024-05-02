4f4ef52250eefc71265b32bbed5df421f81d3d80
EmilioTR <Emilio.TenaRomero@hotmail.com>
2024-05-02 16:25:27 +0200

    working collapsible menus

--- src/components/InteractionLayout/SelectionMenu/SelectionMenu.jsx
+++ src/components/InteractionLayout/SelectionMenu/SelectionMenu.jsx
@@ -1,8 +1,17 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import { useResourceDefinitions } from "ra-core";
 import { DashboardMenuItem } from "ra-ui-materialui";
 import { Menu } from "react-admin";
 import { ThemeProvider, createTheme, Tooltip, Box, Typography } from "@mui/material";
+import config from "../../../config.json";
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import Collapse from '@mui/material/Collapse';
+import ExpandLess from '@mui/icons-material/ExpandLess';
+import ExpandMore from '@mui/icons-material/ExpandMore';
+
 
 /**
  * A custom menu as defined in React Admin for selecting the query the user whishes to execute.
@@ -10,26 +19,46 @@ import { ThemeProvider, createTheme, Tooltip, Box, Typography } from "@mui/mater
  */
 function SelectionMenu() {
   const resources = useResourceDefinitions();
+  const queryGroups = config.queryGroups;
+
+  queryGroups.forEach(group => group.queries = [])
+  setUpQueryGroups(queryGroups, resources)
+
   return (
     <ThemeProvider theme={menuItemTheme}>
       <div style={{ height: '100%', overflowY: 'auto', backgroundColor: 'white' }}>
         <Menu>
           <DashboardMenuItem />
-          {Object.keys(resources).map((id) => (
-            <Tooltip
-              key={id}
-              placement="right"
-              title={
-                <TooltipContent
-                  title={resources[id].options.label}
-                  description={resources[id].options.descr} />
-              }
-            >
-              <div >
-                <Menu.ResourceItem name={id} />
-              </div>
-            </Tooltip>
-          ))}
+          {queryGroups.map((group) => {
+            const [open, setOpen] = useState(false)
+            return (
+              <List key={group.id} >
+                <ListItemButton onClick={() => {setOpen(!open)}}>
+                  <ListItemText primary={group.name} />
+                  {open ? <ExpandLess /> : <ExpandMore />}
+                </ListItemButton>
+                <Collapse in={open} timeout="auto" unmountOnExit>
+                <List component="div" disablePadding>
+                  {group.queries.map((id) => (
+                      <Tooltip
+                          key={id}
+                          placement="right"
+                          title={
+                            <TooltipContent
+                              title={resources[id].options.label}
+                              description={resources[id].options.descr} />
+                          }
+                        >
+                          <ListItem sx={{width: '100%'}} >
+                            <Menu.ResourceItem name={id} />
+                          </ListItem>
+                        </Tooltip>
+                  ))}
+                  </List>
+                </Collapse>
+              </List>
+            )
+          })}
         </Menu>
       </div>
     </ThemeProvider>
@@ -70,7 +99,7 @@ const TooltipContent = ({ title, description }) => (
         backgroundColor: '#6d6d6d',
         paddingX: 1,
         marginX: -1,
-        
+
       }}
     >
       <Typography variant="h6" component="div">
@@ -88,6 +117,20 @@ const TooltipContent = ({ title, description }) => (
     </Box>
   </React.Fragment>
 )
+
+const setUpQueryGroups = (queryGroups, resources) => {
+
+  // this code requires the info in config.json to be correct
+  Object.keys(resources).forEach((id) => {
+    try {
+      queryGroups.find(group => group.id === resources[id].options.queryGroupId).queries.push(id)
+    } catch (error) {
+      throw new Error(`Error adding queries to a group: ${error.message}`);
+    }
+  })
+
+
+}
 
 
 export default SelectionMenu;
