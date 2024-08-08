c387e8d5b6d42195b6861ea18b3665ed5e5ae9da
EmilioTR <Emilio.TenaRomero@hotmail.com>
2024-08-08 15:34:21 +0200

    code is more clear + navigate to the custom form

--- src/components/CustomQueryEditor/customConversionButton.jsx
+++ src/components/CustomQueryEditor/customConversionButton.jsx
@@ -1,135 +1,23 @@
 import { useNavigate } from 'react-router-dom';
-
 import configManager from "../../configManager/configManager";
-import Button from '@mui/material/Button';
 import IconProvider from "../../IconProvider/IconProvider";
-
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
 
 export default function CustomConversionButton({ id }) {
 
     const navigate = useNavigate();
     const config = configManager.getConfig()
-    const queryToConvert = configManager.getQueryById(id);
-    const newId = Date.now().toString()
 
-    const adaptedQuery = {
-        ...queryToConvert,
-        id: newId,
-        queryGroupId: "cstm",
-    }
-
-    // This function replaces the queryLocation with a querystring, which makes it adapted for a custom query
-    async function replaceQueryLocationWithQueryString() {
-
-        const queryString = await configManager.getQueryText(adaptedQuery)
-
-        delete adaptedQuery.queryLocation
-        adaptedQuery.queryString = queryString
-
-
-    }
-
-    // This function handles the conversion of the comunica sources and context
-    function convertComunicaContextAndSources() {
-
-        if (adaptedQuery.comunicaContext) {
-            if (adaptedQuery.comunicaContext.sources) {
-                adaptedQuery.source = adaptedQuery.comunicaContext.sources.join(' ; ')
-            }
-
-            const keys = Object.keys(adaptedQuery.comunicaContext);
-            const otherKeys = keys.filter(key => key !== 'sources');
-
-            const hasOtherProperties = otherKeys.length > 0;
-            if (hasOtherProperties) {
-                adaptedQuery.comunicaContextCheck = "on"
-            }
-        }
-    }
-
-    // This function handles the conversion of indirect sources
-    async function convertSourcesFromSourceIndex() {
-        if (adaptedQuery.sourcesIndex){
-
-            adaptedQuery.sourceIndexCheck = "on";
-
-            const queryString = await configManager.getQueryText(adaptedQuery.sourcesIndex)
-
-            console.log(adaptedQuery.sourcesIndex.url)
-            adaptedQuery.indexSourceUrl = adaptedQuery.sourcesIndex.url
-            adaptedQuery.indexSourceQuery = queryString
-
-            delete adaptedQuery.sourcesIndex.queryLocation
-            adaptedQuery.sourcesIndex.queryString = queryString
-
-        }
-    }
-
-    // This function handles the conversion for fixed variables
-    function convertTemplatedQueriesFixedVariables() {
-        if(adaptedQuery.variables){
-            adaptedQuery.directVariablesCheck = "on"
-        }
-    }
-
-
-    // This function handles the conversion for indirect variables
-    async function convertTemplatedQueriesIndirectVariables() {
-        let queryStringList = [];
-
-        if(adaptedQuery.indirectVariables){
-            adaptedQuery.indirectVariablesCheck = "on"
-
-            if(adaptedQuery.indirectVariables.queryLocations){
-                for (const location of adaptedQuery.indirectVariables.queryLocations) {
-
-                    const result = await fetch(`${config.queryFolder}${location}`);
-                    const queryStr = await result.text();
-            
-                    queryStringList.push(queryStr);
-                  }
-            }   
-            if (adaptedQuery.indirectVariables.queryStrings) {
-                queryStringList = [...queryStringList, ...adaptedQuery.indirectVariables.queryStrings];
-              }
-
-            adaptedQuery.indirectQueries = queryStringList
-
-        }
-    }
-
-    // This function handles the conversion of an ask query
-    function convertASKquery() {
-        if (adaptedQuery.askQuery){
-            adaptedQuery.askQueryCheck = "on"
-        }
-    }
-
-    function handleSearchParams(queryToHandle) {
-
-        const copyObject = JSON.parse(JSON.stringify(queryToHandle));
-
-        if( copyObject.comunicaContextCheck !== "on"){
-            delete copyObject.comunicaContext
-        } else {
-            delete copyObject.comunicaContext.sources
-        }
-
-        for (let content in copyObject) {
-            if (typeof copyObject[content] === 'object') {
-                copyObject[content] = JSON.stringify(copyObject[content])
-            }
-
-        }
-
-        return new URLSearchParams(copyObject);
-    }
+    // We have to make sure we have a deep copy, and not a shallow copy of the original query
+    const convertedQuery = JSON.parse(JSON.stringify(configManager.getQueryById(id)));
 
 
     const convertQueryToCustom = async () => {
 
-
-        // logic
+        /* 
+            The convertion logic is split up in apart functions to enhance its clarity.
+        */
 
         await replaceQueryLocationWithQueryString()
         await convertSourcesFromSourceIndex()
@@ -139,33 +27,127 @@ export default function CustomConversionButton({ id }) {
         convertComunicaContextAndSources()
         convertASKquery()
 
+        // The id and group have to be deleted because a new custom query is going to be made. Name is modified.
+        delete convertedQuery.id
+        delete convertedQuery.queryGroupId
+        convertedQuery.name = `(Derived from) ${convertedQuery.name}`
 
-        
-
-        configManager.addNewQueryGroup('cstm', 'Custom queries', 'EditNoteIcon');
-
-
-        adaptedQuery.searchParams = handleSearchParams(adaptedQuery)
-
-        // const searchParams = new URLSearchParams(adaptedQuery);
-
-        // adaptedQuery.searchParams = searchParams
-
-        configManager.addQuery(adaptedQuery)
-
-        navigate("/" + newId)
+        // Generate the search parameters so that we can create a custom query.
+        const searchParams = handleSearchParams(convertedQuery)
+        navigate(`/customQuery?${searchParams.toString()}`)
     }
 
-    return (
-        <Button
-            variant="outlined"
-            color="warning"
-            onClick={convertQueryToCustom}
-            type="button"
-            startIcon={<IconProvider.SettingsSuggestIcon />}
-        >
-            Customize
-        </Button>
 
+    // Handles the objects parsing and ensure the correct comunica structure is given
+    function handleSearchParams(queryToHandle) {
+
+        const copyObject = JSON.parse(JSON.stringify(queryToHandle));
+
+        for (let content in copyObject) {
+            if (typeof copyObject[content] === 'object') {
+                copyObject[content] = JSON.stringify(copyObject[content])
+            }
+        }
+        return new URLSearchParams(copyObject);
+    }
+
+
+    // This function replaces the queryLocation with a querystring, which makes it adapted for a custom query
+    async function replaceQueryLocationWithQueryString() {
+        const queryString = await configManager.getQueryText(convertedQuery)
+        convertedQuery.queryString = queryString
+        delete convertedQuery.queryLocation
+    }
+
+    // This function handles the conversion of the comunica sources and context
+    function convertComunicaContextAndSources() {
+
+        if (convertedQuery.comunicaContext) {
+            if (convertedQuery.comunicaContext.sources) {
+                convertedQuery.source = convertedQuery.comunicaContext.sources.join(' ; ')
+            }
+
+            const keys = Object.keys(convertedQuery.comunicaContext);
+            const otherKeys = keys.filter(key => key !== 'sources');
+            const hasOtherProperties = otherKeys.length > 0;
+
+            if (hasOtherProperties) {
+                convertedQuery.comunicaContextCheck = "on"
+                delete convertedQuery.comunicaContext.sources
+            } else {
+                delete convertedQuery.comunicaContext
+            }
+
+        }
+    }
+
+    // This function handles the conversion of indirect sources
+    async function convertSourcesFromSourceIndex() {
+        if (convertedQuery.sourcesIndex) {
+
+            convertedQuery.sourceIndexCheck = "on";
+            const queryString = await configManager.getQueryText(convertedQuery.sourcesIndex)
+
+            convertedQuery.indexSourceUrl = convertedQuery.sourcesIndex.url
+            convertedQuery.indexSourceQuery = queryString
+            delete convertedQuery.sourcesIndex.queryLocation
+        }
+    }
+
+    
+    
+    // This function handles the conversion for indirect variables
+    async function convertTemplatedQueriesIndirectVariables() {
+        let queryStringList = [];
+        
+        if (convertedQuery.indirectVariables) {
+            convertedQuery.indirectVariablesCheck = "on"
+            
+            if (convertedQuery.indirectVariables.queryLocations) {
+                for (const location of convertedQuery.indirectVariables.queryLocations) {
+                    
+                    const result = await fetch(`${config.queryFolder}${location}`);
+                    const queryStr = await result.text();
+                    
+                    queryStringList.push(queryStr);
+                }
+            }
+            if (convertedQuery.indirectVariables.queryStrings) {
+                queryStringList = [...queryStringList, ...convertedQuery.indirectVariables.queryStrings];
+            }
+            
+            convertedQuery.indirectQueries = queryStringList
+            
+        }
+    }
+
+    // This function handles the logic for fixed variables
+    function convertTemplatedQueriesFixedVariables() {
+        if (convertedQuery.variables) {
+            convertedQuery.directVariablesCheck = "on"
+        }
+    }
+    
+    // This function handles the logic for the ask query
+    function convertASKquery() {
+        if (convertedQuery.askQuery) {
+            convertedQuery.askQueryCheck = "on"
+        }
+    }
+
+
+    return (
+        <Box display="flex" justifyContent="flex-end" width="100%">
+            <Button
+                variant="outlined"
+                color="warning"
+                onClick={convertQueryToCustom}
+                type="button"
+                startIcon={<IconProvider.SettingsSuggestIcon />}
+                sx={{ marginTop: '10px' , marginBottom: '10px'}}
+            >
+                Customize
+            </Button>
+        </Box>
     )
 }
