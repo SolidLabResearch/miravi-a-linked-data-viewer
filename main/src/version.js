5a2a59441c8988267944abf1df2e71ad6f637120
Martin Vanbrabant <6448245+mvanbrab@users.noreply.github.com>
2025-03-17 10:37:56 +0100

    Prepare release v1.6.0 (#182)

--- main/src/version.js
+++ main/src/version.js
@@ -1,5 +1,5 @@
-// Set to a next intermediate version string vX.Y.Z-tttt when merging an unreleased version to the main branch.
-// Set to the new release version string vX.Y.Z right before make a new release tag vX.Y.Z (see https://github.com/SolidLabResearch/generic-data-viewer-react-admin/tags).
-// ... All in accordance with https://semver.org/spec/v2.0.0.html.
-const version = "v1.6.0.0003";
+// Maintain the version string on the development branch:
+// - during development: set to (unreleased);
+// - right before merging to the main branch for making a new release: set to the new release tag (see https://github.com/SolidLabResearch/generic-data-viewer-react-admin/tags).
+const version = "v1.6.0";
 export default version;
