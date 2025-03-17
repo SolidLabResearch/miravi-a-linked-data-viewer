82e9959d32cb96b3acbaafaad18c4a8d5842f42d
Martin Vanbrabant <martin.vanbrabant@ugent.be>
2025-03-17 09:06:40 +0100

    IDLab link at bottom + copyright

--- main/src/components/InteractionLayout/NavigationBar/AboutDialog.jsx
+++ main/src/components/InteractionLayout/NavigationBar/AboutDialog.jsx
@@ -14,7 +14,7 @@ function AboutDialog(props) {
   return (
     <Dialog open={props.open} onClose={props.close} fullWidth maxWidth="sm">
       <DialogTitle sx={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
-        <div><a href="https://idlab.ugent.be/">IDLab</a> Generic Data Viewer</div>
+        <div>Generic Data Viewer</div>
         <IconButton onClick={props.close} size="small">
           <CloseIcon />
         </IconButton>
@@ -27,6 +27,7 @@ function AboutDialog(props) {
             or mail to <a href="mailto:ben.demeester@ugent.be" target="_blank">ben.demeester@ugent.be</a>.</p>
           <p>Powered by <a href="https://comunica.dev/" target="_blank">Comunica</a> and <a href="https://marmelab.com/react-admin/" target="_blank">React-Admin</a>.</p>
           <p>Initial development funded by <a href="https://ontodeside.eu/" target="_blank">Onto-DESIDE</a> and <a href="https://solidlab.be/" target="_blank">SolidLab</a>.</p>
+          <p>&copy; <a href="https://idlab.ugent.be/">IDLab</a> 2025</p>
         </DialogContentText>
       </DialogContent>
     </Dialog>
