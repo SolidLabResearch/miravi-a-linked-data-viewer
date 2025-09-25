c88218d8304f4977ccd7293a00bae6e521de4a54
Martin Vanbrabant <martin.vanbrabant@ugent.be>
2025-09-25 09:26:35 +0200

    version.js (unreleased)

--- main/src/version.js
+++ main/src/version.js
@@ -1,5 +1,5 @@
 // Maintain the version string on the development branch:
 // - during development: set to unreleased;
 // - right before merging to the main branch for making a new release: set to the new release tag (see https://github.com/SolidLabResearch/miravi-a-linked-data-viewer/tags).
-const version = "v2.1.0";
+const version = "(unreleased)";
 export default version;
